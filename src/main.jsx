2cdccc7bce634cf9a48df2a7ae0eceda885909f2
techcristian <100042922+techcristian@users.noreply.github.com>
2025-07-24 14:44:11 -0300

    dotenv

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { GoogleOAuthProvider } from '@react-oauth/google'
-import dotenv from 'dotenv';
-dotenv.config();
+
 // Reemplazá con tu Client ID de Google Cloud Console
 const clientId = import.meta.env.VITE_CLIENT_ID;
 
