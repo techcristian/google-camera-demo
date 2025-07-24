026b614ae1b9239c750cb8ec5594249890449d97
techcristian <100042922+techcristian@users.noreply.github.com>
2025-07-24 14:40:19 -0300

    dotenv

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import dotenv from 'dotenv';
 dotenv.config();
 // Reemplazá con tu Client ID de Google Cloud Console
-const clientId = process.env.clientId;
+const clientId = import.meta.env.VITE_CLIENT_ID;
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
